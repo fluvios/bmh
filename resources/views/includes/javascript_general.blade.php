8d698776863af6880ad9110eb7e7b7570aca2cad
olid <olidbomber@gmail.com>
2018-03-08 10:40:48 +0700

    Update javascript_general.blade.php
    include curr.js

--- resources/views/includes/javascript_general.blade.php
+++ resources/views/includes/javascript_general.blade.php
@@ -1,21 +1,41 @@
-<!-- Bootstrap core JavaScript 
-    ================================================== -->    
-    <script src="{{ asset('public/plugins/jQuery/jQuery-2.1.4.min.js') }}"></script>
-    <script src="{{ asset('public/js/jquery.easing.1.3.js') }}"></script>
-    <script src="{{ asset('public/js/jquery-ui-1.10.3.custom.min.js') }}"></script>
-    <script src="{{ asset('public/js/jquery.ui.touch-punch.min.js') }}"></script>
-    <script src="{{ asset('public/bootstrap/js/bootstrap.min.js') }}"></script>
-    <script src="{{ asset('public/js/jquery.autosize.min.js') }}"></script>
-    <script src="{{ asset('public/js/jqueryTimeago.js') }}"></script>
-    <script src="{{ asset('public/js/bootbox.min.js') }}"></script>
-    <script src="{{ asset('public/js/count.js') }}"></script>
-    <script src="{{ asset('public/js/functions.js') }}"></script>
-    <script src="{{ asset('public/js/jquery.form.js') }}"></script>
-    <script src="{{ asset('public/plugins/sweetalert/sweetalert.min.js') }}"></script>
-    <script src="{{ asset('public/js/holder.min.js') }}"></script>
-    <script src="{{ asset('public/js/jquery.slides.js') }}"></script>
-    <script src="{{ asset('public/js/cookies.js') }}"></script>
+<!-- Bootstrap core JavaScript 
+
+    ================================================== -->    
+
+    <script src="{{ asset('public/plugins/jQuery/jQuery-2.1.4.min.js') }}"></script>
+
+    <script src="{{ asset('public/js/jquery.easing.1.3.js') }}"></script>
+
+    <script src="{{ asset('public/js/jquery-ui-1.10.3.custom.min.js') }}"></script>
+
+    <script src="{{ asset('public/js/jquery.ui.touch-punch.min.js') }}"></script>
+
+    <script src="{{ asset('public/bootstrap/js/bootstrap.min.js') }}"></script>
+
+    <script src="{{ asset('public/js/jquery.autosize.min.js') }}"></script>
+
+    <script src="{{ asset('public/js/jqueryTimeago.js') }}"></script>
+
+    <script src="{{ asset('public/js/bootbox.min.js') }}"></script>
+
+    <script src="{{ asset('public/js/count.js') }}"></script>
+
+    <script src="{{ asset('public/js/functions.js') }}"></script>
+
+    <script src="{{ asset('public/js/jquery.form.js') }}"></script>
+
+    <script src="{{ asset('public/plugins/sweetalert/sweetalert.min.js') }}"></script>
+ <script src="{{ asset('public/js/curr.js') }}"></script>
+    <script src="{{ asset('public/js/holder.min.js') }}"></script>
+
+    <script src="{{ asset('public/js/jquery.slides.js') }}"></script>
+
+    <script src="{{ asset('public/js/cookies.js') }}"></script>
+   
     <script src="https://cdnjs.cloudflare.com/ajax/libs/typeahead.js/0.11.1/typeahead.bundle.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/corejs-typeahead/1.2.1/bloodhound.min.js"></script>    
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-tagsinput/0.8.0/bootstrap-tagsinput.js"></script>    
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/cleave.js/1.1.1/cleave.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/corejs-typeahead/1.2.1/bloodhound.min.js"></script>    
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-tagsinput/0.8.0/bootstrap-tagsinput.js"></script>    
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/cleave.js/1.1.1/cleave.js"></script>
+
