2b35c68d67c997f83c7bc1e65036e067d292fe70
olid <olidbomber@gmail.com>
2018-04-22 03:09:08 +0700

    Fix Bug

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -5,11 +5,11 @@
 @section('content') 
 
 
-<div class="container margin-bottom-40 margin-top-100">
-	  <h2 class="subtitle-color-5 text-uppercase ">{{ $response->title }}</h2>
+<div class="container margin-bottom-40 margin-top-80">
+     <h2 class="subtitle-color-7 margin-bottom-5  text-uppercase"><a>{{ $response->title }}</a></h2>
 	<div class="row"></div>
 <!-- Col MD -->
-<div class="col-md-12">	
+<div class="col-md-12" >	
 	<ol class="breadcrumb bg-none pull-right">
           	<li><a href="{{ URL::to('/') }}"><i class="glyphicon glyphicon-home myicon-right"></i></a></li>
           	<li class="active">{{ $response->title }}</li>
@@ -17,7 +17,7 @@
 	<hr />
      	</br>
      <dl>
-     	<dd>
+     	<dd class="login-form-2">
      		<?php echo html_entity_decode($response->content) ?>
      	</dd>
      </dl>	
